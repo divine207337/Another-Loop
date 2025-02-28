503bc8bc1c47dc4fd86cc1fe08f751f42ce054db
divine207337 <divinegiftadaugo@gmail.com>
2025-02-28 21:57:24 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,21 @@
+// write a function that prints "Hello World" to the terminal
+function printHelloWorld(){
+    console.log("Hello World")
+} 
+printHelloWorld()
+
+
+function addTwoNumbers(x, y){
+    
+    let result = x + y;
+    console.log(result)
+}
+// addTwoNumbers()
+
+// write a function that multiplies 2 numbers and prints the result to the terminal
+function multiplyTwoNumbers(x, y){
+   
+    let result = x * y;
+    console.log(result)
+}
+multiplyTwoNumbers(5, 7)
